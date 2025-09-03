68d992894135f9561004635bcdcde02b8b3d49e7
chenry22 <112424609+chenry22@users.noreply.github.com>
2025-09-03 01:14:10 -0500

    fix multi page setup

--- web_app/vite.config.js
+++ web_app/vite.config.js
@@ -4,5 +4,12 @@ export default defineConfig({
     base: "/nba-teammate-web/",
     build: {
         outDir: "docs",
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                player_index: 'player_index.html',
+                players: 'players.html',
+            },
+        },
     },
 });
